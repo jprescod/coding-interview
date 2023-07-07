71366476c523e17fb9acdaa91b93366960d4fb7f
Jesse <jprescod@post.harvard.edu>
2023-07-07 11:13:38 -0700

    Got it working!

--- tests/test_palindrome.py
+++ tests/test_palindrome.py
@@ -14,12 +14,19 @@ Sample Response:
     "word": "23444"
 }
 """
+import json
+
+import requests
+
+RACECAR = "racecar"
 
 
 def test_correctly_identifies_a_palindrome():
     """Validate palindrome endpoint correctly identifies a palindrome"""
-    actual_value = None
 
     # Fill in code to fetch the actual is_palindrome value from application
-
-    assert actual_value
+    response = requests.post("http://localhost:8080/palindrome", json={"word": RACECAR})
+    assert response, "No response"
+    assert response.text, "Text is where the response is stored. It's not there!"
+    response = json.loads(response.text)
+    assert response["is_palindrome"], f"{RACECAR} is a palindrome! What gives!"
